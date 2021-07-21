bf87ea52a6789d88153d08dac4f47d712486446c
padenaa <padenarasouli@gmail.com>
2021-07-21 17:58:49 +0000

    end of lesson commit

---
+++ starter/secretMessageApp/public/js/viewMessages.js
@@ -0,0 +1,34 @@
+const getMessages = () => {
+    const messageRef = firebase.database().ref();
+    messageRef.on('value', (snapshot)=>{
+        const data = snapshot.val();
+        console.log(data);
+        for (let key in data){
+            const message = data[key];
+            console.log(message);
+            if(myPass == message.passcode){
+                renderMessage(message);
+            }
+        }
+    });
+}
+
+const findMessage = (myPass) => {
+    const messageRef = firebase.database().ref();
+    messageRef.on('value', (snapshot)=>{
+        const data = snapshot.val();
+    });
+    console.log(data);
+    for (let key in data){
+        const message = data[key];
+        console.log(message);
+        if(myPass == message.passcode){
+            renderMessage(message);
+        }
+    }
+}
+
+document.querySelector("#viewMsg").addEventListener('click', ()=> {
+    const passcode = document.querySelector('#passcode').nodeValue;
+    findMessage(passcode);
+});
